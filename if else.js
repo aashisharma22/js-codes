f5b0f8f85c0a0f2e0a5630496a494bf98e4c3859
Aashish Sharma <96761521+aashisharma22@users.noreply.github.com>
2022-02-28 00:40:27 +0530

    IF else in javascro]ipt

---
+++ if else.js
@@ -0,0 +1,8 @@
+var age = 17;
+         if( age > 18 ) {
+                document.write("<b> ELigible for voting</b>");
+         }
+         else{
+             document.write("<b>Cannot Vote</b>")
+         }
+               
