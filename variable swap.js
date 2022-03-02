a3f7305e474bb0dd6aea848f72133312563beddc
Aashish Sharma <96761521+aashisharma22@users.noreply.github.com>
2022-03-03 01:48:03 +0530

    Swapping variables
    In this code, we swap variables by assignment and the use of a temporary variable.

---
+++ variable swap.js
@@ -0,0 +1,13 @@
+//program to swap variables using a temp variable
+
+let a = 3;
+let b = 5;
+
+let temp;
+
+temp=a;
+a=b;
+b=temp;
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
