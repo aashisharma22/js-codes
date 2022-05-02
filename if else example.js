f6a1c8da1a233fbbd1ef74a145291a645da06c15
Aashish Sharma <96761521+aashisharma22@users.noreply.github.com>
2022-05-02 16:53:36 +0530

    If-else and else if example

---
+++ if else example.js
@@ -0,0 +1,26 @@
+function testSize(num) 
+{
+  if(num<5)
+  {
+    console.log('Tiny');
+  }
+  else if(num<10)
+  {
+    console.log('Small');
+  }
+  else if(num<15)
+  {
+    console.log('Medium');
+  }
+  else if(num<20)
+  {
+    console.log('Large');
+  }
+  else
+  {
+    console.log('Huge');
+  }
+  return "Change Me";
+}
+
+testSize(7);
