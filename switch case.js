89dbe19632c188c3de9ad6e4d11b4579bfd719d8
Aashish Sharma <96761521+aashisharma22@users.noreply.github.com>
2022-05-02 17:46:14 +0530

    Switch Case statement in JavaScript

---
+++ switch case.js
@@ -0,0 +1,25 @@
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+      case 1:
+        console.log('alpha');
+        break;
+      case 2:
+        console.log('beta');
+        break;
+      case 3:
+        console.log('gamma');
+        break;
+      case 4:
+        console.log('delta');
+        break;
+  
+    }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
